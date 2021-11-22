9db2f0f22ff833d3be5a78ed263b0d7333783cf1
Joar Christoffersen <jc@fjellsport.no>
2021-11-22 16:18:06 +0100

    added links

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,5 @@
 import Container from './container'
-import { EXAMPLE_PATH } from '../lib/constants'
+import { EXAMPLE_PATH, LINK_PATH } from '../lib/constants'
 
 export default function Footer() {
   return (
@@ -11,10 +11,10 @@ export default function Footer() {
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
-              href="https://nextjs.org/docs/basic-features/pages"
+              href={LINK_PATH} target="_blank"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
-              All content copyright Team Topout
+              Check boulders nearby
             </a>
             <a
               href={EXAMPLE_PATH} target="_blank"
