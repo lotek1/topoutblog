d4ccd4ea991778957ff61d3a25a052b1e5b44685
Joar Christoffersen <jc@fjellsport.no>
2021-12-06 08:30:55 +0100

    Removed logo setup

--- components/header.js
+++ components/header.js
@@ -1,19 +1,10 @@
 import Link from 'next/link'
-import Image from 'next/image'
-import logo from '../public/topoutlogo.png'
 
 export default function Header() {
   return (
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
       <Link href="/">
-        <a className="hover:underline">
-         <Image
-         src={logo} 
-         alt="Logo for Topout"
-         width={500}
-         height={500}
-         />
-          </a>
+        <a className="hover:underline">Topout</a>
       </Link>
       .
     </h2>
